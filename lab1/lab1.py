6ed7a46ff5be536b2f59ce6071788bc5f204972f
saccuz <marco.sacchet.97@gmail.com>
2022-10-13 17:36:02 +0200

    Made a proper file structure and fixed forgotten wrong import

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -1,7 +1,7 @@
-from copy import copy
 import random
 from types import NoneType
-import numpy as np
+
+from search import tree_explorer_DF
 
 def problem(N, seed=None):
     random.seed(seed)
@@ -10,42 +10,12 @@ def problem(N, seed=None):
         for n in range(random.randint(N, N * 5))
     ]
 
-def check_solution(solution, N):
-    if len(solution) == 0:
-        return (False, solution, 0)
-    check_ = np.ones(N)*(N+1) #vettore di lunghezza N, se la soluzione e' giusta ogni elemento avra' valore pari al suo indice
-    collision = 0
-    for el in solution:
-        for i in el:
-            if check_[i] == i: #se il valore e' gia' presente segno la collisione
-                collision += 1
-            else:
-                check_[i] = i
-    if any(j == N+1 for j in check_): #controllo che tutti gli elementi siano stati settati
-        return (False, solution, collision)
-    else:
-        return (True, solution, collision)
-
-def tree_explorer(sol, space, N):
-    local_s = copy(sol)
-    isCorrect = check_solution(sol, N)
-    if  len(local_s) != 0 and len(local_s[-1]) <= isCorrect[2]: #controllo che il numero di collisioni sia minore della lunghezza della lista
-        sol.pop(-1)
-    if isCorrect[0]:
-        return isCorrect
-    for l in space:
-        local_s.append(l)
-        ind_l = space.index(l)
-        ctrl = tree_explorer(local_s, space[ind_l+1 : ], N)
-        if ctrl is not None and ctrl[0]:
-            return ctrl
-
 if __name__ == "__main__":
     seed = 42
     for N in [5, 10, 20, 100, 500, 1000]:
         space = problem(N, seed)
         space.sort(key=len, reverse=True)
-        solution = tree_explorer([], space, N)
+        solution = tree_explorer_DF([], space, N)
         print(f"Solution for {N}: {len(solution[1])}")
 
 
---
+++ lab1/search.py
@@ -0,0 +1,32 @@
+from copy import copy
+import numpy as np
+
+def check_solution(solution, N):
+    if len(solution) == 0:
+        return (False, solution, 0)
+    check_ = np.ones(N)*(N+1) #vettore di lunghezza N, se la soluzione e' giusta ogni elemento avra' valore pari al suo indice
+    collision = 0
+    for el in solution:
+        for i in el:
+            if check_[i] == i: #se il valore e' gia' presente segno la collisione
+                collision += 1
+            else:
+                check_[i] = i
+    if any(j == N+1 for j in check_): #controllo che tutti gli elementi siano stati settati
+        return (False, solution, collision)
+    else:
+        return (True, solution, collision)
+
+def tree_explorer_DF(sol, space, N):
+    local_s = copy(sol)
+    isCorrect = check_solution(sol, N)
+    if  len(local_s) != 0 and len(local_s[-1]) <= isCorrect[2]: #controllo che il numero di collisioni sia minore della lunghezza della lista
+        sol.pop(-1)
+    if isCorrect[0]:
+        return isCorrect
+    for l in space:
+        local_s.append(l)
+        ind_l = space.index(l)
+        ctrl = tree_explorer_DF(local_s, space[ind_l+1 : ], N)
+        if ctrl is not None and ctrl[0]:
+            return ctrl
