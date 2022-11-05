b5ffdba05e1b34f059e70d010ffc6cd53550a88f
saccuz <marco.sacchet.97@gmail.com>
2022-11-05 02:32:50 +0100

    Iteration for all the N

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -13,19 +13,19 @@ if __name__ == "__main__":
 
     NUM_GENERATIONS = 1000
 
-    N=50
-
     POPULATION_SIZE = 30
     OFFSPRING_SIZE = 20 
 
     seed = 42
-    space = list( _ for _ in set(tuple(sorted(set(_))) for _ in problem(N, seed)))
 
-    how_many_covered, collisions, weight =search.evolutionary_solution(N, space, POPULATION_SIZE, OFFSPRING_SIZE, NUM_GENERATIONS)
+    for N in [5, 10, 50, 100, 500, 1000, 2000, 5000]:
+        space = list( _ for _ in set(tuple(sorted(set(_))) for _ in problem(N, seed)))
 
-    print(f"How many covered: {how_many_covered}")
-    print(f"Collisions: {collisions}")
-    print(f"Weight: {weight}")
-    print(f"Population Size: {POPULATION_SIZE}")
-    print(f"Offspring Size: {OFFSPRING_SIZE}")
-    print(f"Generations: {NUM_GENERATIONS}")
+        how_many_covered, collisions, weight = search.evolutionary_solution(N, space, POPULATION_SIZE, OFFSPRING_SIZE, NUM_GENERATIONS)
+
+        print(f"How many covered: {how_many_covered}")
+        print(f"Collisions: {collisions}")
+        print(f"Weight: {weight}")
+        print(f"Population Size: {POPULATION_SIZE}")
+        print(f"Offspring Size: {OFFSPRING_SIZE}")
+        print(f"Generations: {NUM_GENERATIONS}")
