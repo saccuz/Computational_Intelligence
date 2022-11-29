da084f227297479fb4e43b1b45b1eb23974e0294
saccuz <marco.sacchet.97@gmail.com>
2022-11-29 11:34:41 +0100

    Reordered code Strategy for the test opponent

--- lab3/lab3.py
+++ lab3/lab3.py
@@ -7,25 +7,11 @@ from nim import Nim, Nimply
 
 # Sample (and silly) strategies
 
-def pure_random(state: Nim) -> Nimply:
-    row = random.choice([r for r, c in enumerate(state.rows) if c > 0])
-    num_objects = random.randint(1, state.rows[row])
-    return Nimply(row, num_objects)
-
-def gabriele(state: Nim) -> Nimply:
-    """Pick always the maximum possible number of the lowest row"""
-    possible_moves = [(r, o) for r, c in enumerate(state.rows) for o in range(1, c + 1)]
-    return Nimply(*max(possible_moves, key=lambda m: (-m[0], m[1])))
-
 def nim_sum(state: Nim) -> int:
     result = state.rows[0]
     for row in state.rows[1:]:
         result = result ^ row
     return result
-    
-    #tmp = np.array([tuple(int(x) for x in f"{c:032b}") for c in state.rows])
-    #xor = tmp.sum(axis=0) % 2
-    #return int("".join(str(_) for _ in xor), base=2)
 
 def cook_status(state: Nim) -> dict:
     cooked = dict()
@@ -49,6 +35,34 @@ def optimal_strategy(state: Nim) -> Nimply:
     #return next(m for m in data['possible_moves'] if m[1] == data['min_sum'])
     return next((bf for bf in data['brute_force'] if bf[1] == 0), random.choice(data['brute_force']))[0]
 
+def pure_random(state: Nim) -> Nimply:
+    row = random.choice([r for r, c in enumerate(state.rows) if c > 0])
+    num_objects = random.randint(1, state.rows[row])
+    return Nimply(row, num_objects)
+
+def gabriele(state: Nim) -> Nimply:
+    """Pick always the maximum possible number of the lowest row"""
+    possible_moves = [(r, o) for r, c in enumerate(state.rows) for o in range(1, c + 1)]
+    return Nimply(*max(possible_moves, key=lambda m: (-m[0], m[1])))
+
+def shortest_row(state: Nim) -> Nimply:
+    data = cook_status(state)
+    return Nimply(data['shortest_row'], random.randint(1, state.rows[data['shortest_row']]))
+
+def longest_row(state: Nim) -> Nimply:
+    data = cook_status(state)
+    return Nimply(data['longest_row'], random.randint(1, state.rows[data['longest_row']]))
+
+def opponent_strategy(turn: int):
+    if turn % 2 == 0:
+        return optimal_strategy
+    else:
+        return pure_random
+    
+    #tmp = np.array([tuple(int(x) for x in f"{c:032b}") for c in state.rows])
+    #xor = tmp.sum(axis=0) % 2
+    #return int("".join(str(_) for _ in xor), base=2)
+
 def make_strategy(genome: dict) -> Callable:
     def evolvable(state: Nim) -> Nimply:
         data = cook_status(state)
@@ -67,7 +81,6 @@ NUM_MATCHES = 100
 NIM_SIZE=10
 
 def evaluate(strategy: Callable) -> float:
-    #opponent = (strategy, pure_random)
     opponent = (strategy, optimal_strategy)
     won = 0
 
@@ -82,7 +95,23 @@ def evaluate(strategy: Callable) -> float:
             won += 1
     return won/NUM_MATCHES
 
-#Testing
+def evaluate_with_average(strategy: Callable) -> float:
+    won = 0
+    for m in range(NUM_MATCHES):
+        nim = Nim(NIM_SIZE)
+        player = 0
+        i = 0
+        while nim:
+            opponent = (strategy,opponent_strategy(i))
+            ply = opponent[player](nim)
+            nim.nimming(ply)
+            player = 1 - player
+            i+=1
+        if player == 1:
+            won += 1
+    return won/NUM_MATCHES
+
+
 if __name__ == "__main__":
     evaluate(make_strategy({'p' : .1}))
 
